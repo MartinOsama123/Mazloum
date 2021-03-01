72034125e807ea800b7b91c9035f5f6540bcdb30
Martin Osama <martinosama2015@gmail.com>
2021-03-01 11:57:15 +0200

    Added and switched to Montserrat font family

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
-
+        fontFamily: 'Montserrat',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: SupplierRegisteration(),

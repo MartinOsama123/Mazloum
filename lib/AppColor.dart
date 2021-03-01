c4b56952790e7dfcf123c0cfed0419ab3ea86220
Martin Osama <martinosama2015@gmail.com>
2021-03-01 12:06:05 +0200

    Added Colors used as const in separate class

---
+++ lib/AppColor.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColor {
+  static const PrimaryColor = Color(0xFF0f6388);
+  static const SecondColor = Color(0xFF527686);
+  static const AccentColor = Color(0xFF60c33c);
+  static const YellowColor = Color(0xFF60c33c);
+}

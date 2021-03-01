bb425e7855616d180e9f52fcf900bc4c021cda32
Martin Osama <martinosama2015@gmail.com>
2021-03-01 15:43:08 +0200

    Added Floating Add Button

--- lib/Screens/VendorMainPage/VendorMainPage.dart
+++ lib/Screens/VendorMainPage/VendorMainPage.dart
@@ -6,6 +6,7 @@ class VendorMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add,color: Colors.white,size: 32,),elevation: 3,),
       body: SafeArea(
         child: Center(
           child: Column(
@@ -46,68 +47,9 @@ class VendorMainScreen extends StatelessWidget {
                     children: [
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: AppColor.HorizontalPadding,vertical: 10),
-                        child: Text("AAAA",style: TextStyle(fontSize: 14,color: AppColor.SecondColor,fontWeight: FontWeight.bold))
+                        child: Text("Industry Type",style: TextStyle(fontSize: 14,color: AppColor.SecondColor,fontWeight: FontWeight.bold))
                       ),
-                      Container(
-                          height: 115,
-                          child: ListView.builder(
-                            itemCount: 4,
-                            shrinkWrap: true,
-                            physics: ClampingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 14),
-                                child: Card(
-                                  elevation: 3,
-                                  shadowColor: Color(0xFF1a0f6388),
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                                  child: Container(
-                                    height: 115,
-                                    width: 320,
-                                    child: Column(
-                                      children: [
-                                        ListTile(
-                                          leading: Image.asset("images/temp_logo.png"),
-                                          title: Text("Company Name",style: TextStyle(fontSize: 14,color: AppColor.SecondColor,fontWeight: FontWeight.bold)),
-                                          subtitle: Text("Industry Name",style: TextStyle(fontSize: 14,color: AppColor.SilverColor)),
-                                          trailing: RatingBar.builder(
-                                            itemSize: 15,
-                                            initialRating: 3,
-                                            minRating: 1,
-                                            direction: Axis.horizontal,
-                                            allowHalfRating: true,
-                                            itemCount: 5,
-
-                                            itemBuilder: (context, _) =>
-                                                Icon(
-                                                  Icons.star,
-                                                  color: Colors.amber,
-                                                ),
-                                            onRatingUpdate: (rating) {
-                                              print(rating);
-                                            },
-                                          ),
-                                        ),
-
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
-                                          child: Center(child:
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.center,
-                                            children: [
-                                              Icon(Icons.people,color: AppColor.SecondColor,),
-                                              Text("1-10 Employee(s)",style: TextStyle(fontSize: 14,color: AppColor.SecondColor))
-                                            ],),),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
+                      SupplierCardInfo(),
 
                     ],
                   ),
@@ -122,4 +64,73 @@ class VendorMainScreen extends StatelessWidget {
     );
   }
 
+}
+
+class SupplierCardInfo extends StatelessWidget {
+  const SupplierCardInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 115,
+        child: ListView.builder(
+          itemCount: 4,
+          shrinkWrap: true,
+          physics: ClampingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14),
+              child: Card(
+                elevation: 3,
+                shadowColor: Color(0xFF1a0f6388),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                child: Container(
+                  height: 115,
+                  width: 320,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Image.asset("images/temp_logo.png"),
+                        title: Text("Company Name",style: TextStyle(fontSize: 14,color: AppColor.SecondColor,fontWeight: FontWeight.bold)),
+                        subtitle: Text("Industry Name",style: TextStyle(fontSize: 14,color: AppColor.SilverColor)),
+                        trailing: RatingBar.builder(
+                          itemSize: 15,
+                          initialRating: 3,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          ignoreGestures: true,
+                          itemCount: 5,
+                          itemBuilder: (context, _) =>
+                              Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                          onRatingUpdate: (rating) {
+                            print(rating);
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                        child: Center(child:
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.people,color: AppColor.SecondColor,),
+                            Text("1-10 Employee(s)",style: TextStyle(fontSize: 14,color: AppColor.SecondColor))
+                          ],),),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      );
+  }
 }

6d6403776ca01108e6da813e4e7c847abe3631e9
Martin Osama <martinosama2015@gmail.com>
2021-03-02 10:11:24 +0200

    Created CompanyModel data

---
+++ lib/Models/CompanyModel.dart
@@ -0,0 +1,11 @@
+class CompanyModel {
+  String companyName;
+  List<String> industryTypes;
+  int ratingNumber;
+  int employeeNumber;
+  String description;
+  String profileURL;
+  String logoURL;
+  List<String> contacts;
+  List<String> emails;
+}

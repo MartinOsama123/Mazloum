bbad0250c694002be3701e1a8b5c333cdb516e9b
Martin Osama <martinosama2015@gmail.com>
2021-03-02 12:09:21 +0200

    Supplier Profile Screen is now colored and formatted

--- lib/Screens/SupplierProfile/SupplierProfile.dart
+++ lib/Screens/SupplierProfile/SupplierProfile.dart
@@ -7,46 +7,106 @@ class SupplierProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Company Profile",style: TextStyle(color: AppColor.PrimaryColor),),centerTitle: true,backgroundColor: Colors.transparent,elevation: 0,),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: AppColor.HorizontalPadding,horizontal: AppColor.HorizontalPadding),
-        child: Center(
-          child: Column(children: [
-            Image.asset("images/temp_logo.png",height: 154),
-            Text("Company Name"),
-            Text("Industry Name"),
-            RatingBar.builder(
-              itemSize: 20,
-              initialRating: 3,
-              minRating: 1,
-              direction: Axis.horizontal,
-              allowHalfRating: true,
-              ignoreGestures: true,
-              itemCount: 5,
-              itemBuilder: (context, _) =>
-                  Icon(
-                    Icons.star,
-                    color: Colors.amber,
+      appBar: PreferredSize(preferredSize: Size.fromHeight(40),child: AppBar(title: Text("Company Profile",style: TextStyle(color: AppColor.SecondColor,fontSize: 16,fontWeight: FontWeight.bold),),iconTheme: IconThemeData(
+        color: AppColor.PrimaryColor, //change your color here
+      ),centerTitle: true,backgroundColor: Colors.transparent,elevation: 0)),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0,horizontal: AppColor.HorizontalPadding),
+              child: Column(children: [
+                Image.asset("images/temp_logo.png",height: 154),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Text("Company Name",style: TextStyle(color: AppColor.PrimaryColor,fontSize: 14,fontWeight: FontWeight.bold)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                  child: Text("Industry Name",style: TextStyle(color: AppColor.SilverColor,fontSize: 14)),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                  child: RatingBar.builder(
+                    itemSize: 20,
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    ignoreGestures: true,
+                    itemCount: 5,
+                    itemBuilder: (context, _) =>
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                    onRatingUpdate: (rating) {
+                      print(rating);
+                    },
                   ),
-              onRatingUpdate: (rating) {
-                print(rating);
-              },
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center,children: [
-              Icon(Icons.people),
-              Text("1-10 Employee")
-            ],),
-            Text("Description"),
-            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
-            Text("To Know More"),
-            FlatButton.icon(onPressed: (){}, icon: Icon(Icons.file_download), label: Text("Download Portfolio")),
-            Row(children: [
-              FlatButton.icon(onPressed: (){}, icon: Icon(Icons.phone), label: Text("")),
-              FlatButton.icon(onPressed: (){}, icon: Icon(Icons.mail), label: Text("Mail")),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, AppColor.HorizontalPadding),
+                  child: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Icon(Icons.people,color: AppColor.SecondColor),
+                    ),
+                    Text("1-10 Employee",style: TextStyle(fontSize: 14,color: AppColor.SecondColor),)
+                  ],),
+                ),
+                Align(child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  child: Text("Description",style: TextStyle(color: AppColor.PrimaryColor,fontSize: 14,fontWeight: FontWeight.bold)),
+                ),alignment: Alignment.centerLeft,),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, AppColor.HorizontalPadding),
+                  child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",style: TextStyle(color: AppColor.SecondColor,fontSize: 14,fontWeight: FontWeight.w500)),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 12),
+                    child: Text("To Know More",style: TextStyle(color: AppColor.SecondColor,fontSize: 14,fontWeight: FontWeight.bold)),
+                  ),
+                ),
+                FlatButton.icon(onPressed: (){}, label: const Text("DOWNLOAD PORTFOLIO",style: TextStyle(fontWeight: FontWeight.bold),), icon: Icon(Icons.file_download),shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5), side: BorderSide(color: AppColor.PrimaryColor,width: 1)
+                ),
+                  minWidth: MediaQuery.of(context).size.width * 0.81,
+                  height: MediaQuery.of(context).size.height * 0.076,
+                  textColor: AppColor.PrimaryColor,
+                ),
+                Align(child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0,12,0,8),
+                  child: Text("Contact Info",style: TextStyle(color: AppColor.SecondColor,fontSize: 14,fontWeight: FontWeight.bold)),
+                ),alignment: Alignment.centerLeft),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0,0,0,12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Ink(
+                        width: 72,
+                      height: MediaQuery.of(context).size.height * 0.076,
+                        decoration: ShapeDecoration(
+                          color: Color(0xFF0f8817),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.phone),
+                          color: Colors.white,
+                          onPressed: () {},
+                        ),
+                      ),
+                    FlatButton.icon(onPressed: (){}, icon: Icon(Icons.mail), label: Text("Mail"),textColor: Colors.white,color: AppColor.PrimaryColor,minWidth: MediaQuery.of(context).size.width * 0.6,height: MediaQuery.of(context).size.height * 0.076,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
 
-            ],)
+                  ],),
+                )
 
-          ],),
+              ],),
+
+          ),
         ),
       ),
     );
--- lib/Screens/VendorMainPage/VendorMainPage.dart
+++ lib/Screens/VendorMainPage/VendorMainPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vendors/AppColor.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:vendors/Screens/SupplierProfile/SupplierProfile.dart';
 
 class VendorMainScreen extends StatelessWidget {
   @override
@@ -49,8 +50,18 @@ class VendorMainScreen extends StatelessWidget {
                         padding: const EdgeInsets.symmetric(horizontal: AppColor.HorizontalPadding,vertical: 10),
                         child: Text("Industry Type",style: TextStyle(fontSize: 14,color: AppColor.SecondColor,fontWeight: FontWeight.bold))
                       ),
-                      SupplierCardInfo(),
-
+                      Container(
+                        height: 115,
+                        child: ListView.builder(
+                          itemCount: 4,
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return SupplierCardView();
+                          },
+                        ),
+                      )
                     ],
                   ),
                 );
@@ -66,71 +77,68 @@ class VendorMainScreen extends StatelessWidget {
 
 }
 
-class SupplierCardInfo extends StatelessWidget {
-  const SupplierCardInfo({
+class SupplierCardView extends StatelessWidget {
+  const SupplierCardView({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 115,
-        child: ListView.builder(
-          itemCount: 4,
-          shrinkWrap: true,
-          physics: ClampingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 14),
-              child: Card(
-                elevation: 3,
-                shadowColor: Color(0xFF1a0f6388),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                child: Container(
-                  height: 115,
-                  width: 320,
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: Image.asset("images/temp_logo.png"),
-                        title: Text("Company Name",style: TextStyle(fontSize: 14,color: AppColor.SecondColor,fontWeight: FontWeight.bold)),
-                        subtitle: Text("Industry Name",style: TextStyle(fontSize: 14,color: AppColor.SilverColor)),
-                        trailing: RatingBar.builder(
-                          itemSize: 15,
-                          initialRating: 3,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          ignoreGestures: true,
-                          itemCount: 5,
-                          itemBuilder: (context, _) =>
-                              Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                              ),
-                          onRatingUpdate: (rating) {
-                            print(rating);
-                          },
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SupplierProfile()),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14),
+        child: Card(
+          elevation: 3,
+          shadowColor: Color(0xFF1a0f6388),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          child: Container(
+            height: 115,
+            width: 320,
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Image.asset("images/temp_logo.png"),
+                  title: Text("Company Name",style: TextStyle(fontSize: 14,color: AppColor.SecondColor,fontWeight: FontWeight.bold)),
+                  subtitle: Text("Industry Name",style: TextStyle(fontSize: 14,color: AppColor.SilverColor)),
+                  trailing: RatingBar.builder(
+                    itemSize: 15,
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    ignoreGestures: true,
+                    itemCount: 5,
+                    itemBuilder: (context, _) =>
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
-                        child: Center(child:
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.people,color: AppColor.SecondColor,),
-                            Text("1-10 Employee(s)",style: TextStyle(fontSize: 14,color: AppColor.SecondColor))
-                          ],),),
-                      )
-                    ],
+                    onRatingUpdate: (rating) {
+                      print(rating);
+                    },
                   ),
                 ),
-              ),
-            );
-          },
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                  child: Center(child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.people,color: AppColor.SecondColor,),
+                      Text("1-10 Employee(s)",style: TextStyle(fontSize: 14,color: AppColor.SecondColor))
+                    ],),),
+                )
+              ],
+            ),
+          ),
         ),
-      );
+      ),
+    );
   }
 }

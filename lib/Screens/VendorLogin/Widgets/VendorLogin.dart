b23917c35123862f7aef36b1798c065c42acb451
Martin Osama <martinosama2015@gmail.com>
2021-03-03 11:35:24 +0200

    Vendor Login pic are now filling whole container

--- lib/Screens/VendorLogin/Widgets/VendorLogin.dart
+++ lib/Screens/VendorLogin/Widgets/VendorLogin.dart
@@ -11,73 +11,78 @@ class _VendorLoginScreenState extends State<VendorLoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                  height: 250,
-                  child: Image.asset("images/group_359.png")),
-              Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: Text(
-                  "Find your supplier now",
-                  style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: AppColor.PrimaryColor),
-                  textAlign: TextAlign.center,
+      body: Column(
+        children: [
+          Container(
+              height: 250,
+              child: FittedBox(fit: BoxFit.fill,child: Image.asset("images/group_359.png")),width: MediaQuery.of(context).size.width),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: AppColor.HorizontalPadding),
+              child: Column(
+                  children: [
+
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Text(
+                        "Find your supplier now",
+                        style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: AppColor.PrimaryColor),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal,color: AppColor.SecondColor),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    // Spacer(),
+                    // SupplierMainInfo(),
+                    // Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+
+                      )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          )),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Next",
+                          style: TextStyle(
+                              fontSize: 18, letterSpacing: 1, color: Colors.white),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)
+                        ),
+                        minWidth: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.07,
+                        color: AppColor.PrimaryColor,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
-                child: Text(
-                  "Login",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal,color: AppColor.SecondColor),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              // Spacer(),
-              // SupplierMainInfo(),
-              // Spacer(),
-              Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-         
-                )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    )),
-              ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Next",
-                    style: TextStyle(
-                        fontSize: 18, letterSpacing: 1, color: Colors.white),
-                  ),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5)
-                  ),
-                  minWidth: MediaQuery.of(context).size.width * 0.81,
-                  height: MediaQuery.of(context).size.height * 0.07,
-                  color: AppColor.PrimaryColor,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
+
     );
   }
 }

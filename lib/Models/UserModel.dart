185380095b286fd22165b030e46cda9af2ce0566
Martin Osama <martinosama2015@gmail.com>
2021-11-02 16:20:57 +0200

    Added UserModel

--- lib/Models/UserModel.dart
+++ lib/Models/UserModel.dart
@@ -1,3 +1,9 @@
 class UserModel {
   late String firstName;
+  late String lastName;
+  late String password;
+  late String email;
+  late String phone;
+
+  UserModel(this.firstName, this.lastName, this.password, this.email, this.phone);
 }

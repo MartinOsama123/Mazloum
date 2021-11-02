441a5f3a5dd377ee5c99af1b79bad3d1266445aa
Martin Osama <martinosama2015@gmail.com>
2021-11-02 16:26:10 +0200

    Added Json encoding and decoding to UserModel

--- lib/Models/UserModel.dart
+++ lib/Models/UserModel.dart
@@ -6,4 +6,20 @@ class UserModel {
   late String phone;
 
   UserModel(this.firstName, this.lastName, this.password, this.email, this.phone);
+  UserModel.fromJson(Map<String, dynamic> json){
+    firstName = json['firstName'];
+    lastName = json['lastName'];
+    password = json['password'];
+    email = json['email'];
+    phone = json['phone'];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['firstName'] = this.firstName;
+    data['lastName'] = this.lastName;
+    data['password'] = this.password;
+    data['email'] = this.email;
+    data['phone'] = this.phone;
+    return data;
+  }
 }
